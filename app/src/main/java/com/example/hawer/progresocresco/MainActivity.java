989b8736339c17fe45a2e82884c136812b494b35
Hawerd <82hags@gmail.com>
2018-08-12 20:37:23 -0500

    Copia de como hacer una barra de progreso circular

--- app/src/main/java/com/example/hawer/progresocresco/MainActivity.java
+++ app/src/main/java/com/example/hawer/progresocresco/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.hawer.progresocresco;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,35 +14,52 @@ public class MainActivity extends AppCompatActivity {
 
     private int cantidadProgreso = 0;
     private ProgressBar progressBar;
-    private Button sumarButton;
+    private Button sumarButton, irmainButton;
     private TextView labelProgresso;
+    private ConstraintLayout layoutProgresoRetos, layoutRetosFinalizados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_card_view);
 
-//        progressBar = findViewById(R.id.progressbar_retos);
-//        sumarButton = findViewById(R.id.botonSumar);
-//        labelProgresso = findViewById(R.id.label_progreso);
-//        sumarButton.setOnClickListener( onButtonClick );
-//
-//        labelProgresso.setText( String.valueOf(cantidadProgreso) );
+        layoutProgresoRetos = findViewById(R.id.contendor_layout_contenido);
+        layoutRetosFinalizados = findViewById(R.id.contendor_layout_retos_finzalizados);
+
+        progressBar = findViewById(R.id.progressbar_retos);
+        sumarButton = findViewById(R.id.botonSumar);
+        irmainButton = findViewById(R.id.irmain);
+        labelProgresso = findViewById(R.id.label_cantidad_progreso);
+        sumarButton.setOnClickListener(onButtonClick);
+        irmainButton.setOnClickListener(onButtonClick);
+
+        labelProgresso.setText(String.valueOf(cantidadProgreso) + "%");
     }
 
     private View.OnClickListener onButtonClick = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
 
-            cantidadProgreso = cantidadProgreso + 10;
-            if(cantidadProgreso <=100){
-                progressBar.setProgress(cantidadProgreso);
+            switch (view.getId()){
+                case R.id.botonSumar:
+                    if (cantidadProgreso < 100) {
+                        cantidadProgreso = cantidadProgreso + 10;
+                        progressBar.setProgress(cantidadProgreso);
+                        labelProgresso.setText(String.valueOf(cantidadProgreso) + "%");
+                    }
+
+                    if (cantidadProgreso == 100) {
+                        layoutProgresoRetos.setVisibility(View.GONE);
+                        layoutRetosFinalizados.setVisibility(View.VISIBLE);
+                        Toast.makeText(MainActivity.this, "Felicidades", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case R.id.irmain:
+                    startActivity(new Intent(MainActivity.this, Main.class));
+                    break;
             }
 
-            if(cantidadProgreso == 100){
-                Toast.makeText(MainActivity.this, "Felicidades", Toast.LENGTH_SHORT).show();
-            }
-            labelProgresso.setText( String.valueOf(cantidadProgreso) );
+
         }
     };
 }
--- app/src/main/java/com/example/hawer/progresocresco/login/Login.java
+++ app/src/main/java/com/example/hawer/progresocresco/login/Login.java
@@ -1,12 +1,15 @@
 package com.example.hawer.progresocresco.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
+import com.example.hawer.progresocresco.MainActivity;
 import com.example.hawer.progresocresco.R;
 
 public class Login extends AppCompatActivity {
@@ -16,6 +19,14 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        Button loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, MainActivity.class));
+            }
+        });
 
     }
 
